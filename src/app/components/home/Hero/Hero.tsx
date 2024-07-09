71c23342fc8e1d552df3a48bdf7130c53f815561
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-07-09 12:51:43 -0400

    Class 10: CSS Modules

--- src/app/components/home/Hero/Hero.tsx
+++ src/app/components/home/Hero/Hero.tsx
@@ -1,7 +1,12 @@
+import styles from './hero.module.css';
+
 export const Hero = () => {
+  console.log(styles);
+
   return (
-    <section>
-      <h1>Hero</h1>
+    <section className={styles.Hero}>
+      <h1>Future world</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
     </section>
   );
 };
